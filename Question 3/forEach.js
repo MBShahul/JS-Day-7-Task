7dd8f684558a66f24cd4e1c4ee585c30c4028b82
MBShahul <iamshahulms6@gmail.com>
2024-06-04 22:59:04 +0530

    second commit

--- Question 3/forEach.js
+++ Question 3/forEach.js
@@ -2,7 +2,11 @@ function reqListener() {
     let countryArr = JSON.parse(this.responseText);
     countryArr.forEach(element => {
         console.log("Name:", element.name.common);
-        console.log("Capital:", element.capital[0]);
+        if (element.capital && Array.isArray(element.capital) && element.capital.length > 0) {
+            console.log("Capital:", element.capital[0]);
+        } else {
+            console.log("Capital: N/A");
+        }
         console.log("Flag:", element.flags.png);
         console.log(`\n`);
     });
@@ -12,6 +16,3 @@ const req = new XMLHttpRequest();
 req.addEventListener("load", reqListener);
 req.open("GET", "https://restcountries.com/v3.1/all");
 req.send();
-
-
-
